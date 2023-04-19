2640afc75bbfcde455c6e699ce6448bca10ff2f6
jorge silva <jorgelafaiete.silva@gmail.com>
2023-04-19 22:29:32 +0100

    Initial commit

---
+++ src/NavegadorDeInternet/Browse.java
@@ -0,0 +1,2 @@
+package NavegadorDeInternet;public class Browse {
+}
---
+++ src/Service/Iphone.java
@@ -0,0 +1,8 @@
+package Service;
+
+import AparelhoTelefonico.AparelhoTelefonico;
+import NavegadorDeInternet.NavegadorDeInternet;
+import ReprodutorMusical.ReprodutorMusical;
+
+public class IphoneInterface implements ReprodutorMusical, AparelhoTelefonico, NavegadorDeInternet {
+}
